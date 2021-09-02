dec763104dc8b0f51107e370577bd590dcdf3c53
Lee Hyun Jun <mikey1000@naver.com>
2021-09-02 17:17:20 +0900

    POST로 DB save

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import HelloWorld
+
+
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+        # POST에서 'hello_world_input' 라는 데이터를 가져오기
+        temp = request.POST.get('hello_world_input')
+
+        # models.py에서 model 가져오기
+        new_hello_world = HelloWorld()
+        # input 받은 데이터를 HelloWorld 모델의 text 라는 속성에 저장
+        new_hello_world.text = temp
+        # 저장
+        new_hello_world.save()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
+
