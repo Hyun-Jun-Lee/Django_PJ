e2d08f24bcccd8ee62c9bcd2809259b8edfa30b8
Lee Hyun Jun <mikey1000@naver.com>
2021-09-03 01:36:00 +0900

    bootstrap & font

--- stay/settings.py
+++ stay/settings.py
@@ -50,7 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountapp'
+    'accountapp',
+    'bootstrap4'
+
 ]
 
 MIDDLEWARE = [
