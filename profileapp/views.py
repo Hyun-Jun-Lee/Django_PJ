7ca104e091eb52e49830d786923be3eb32d84041
Lee Hyun Jun <mikey1000@naver.com>
2021-09-04 00:12:11 +0900

    profileapp END

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -12,7 +14,7 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -21,3 +23,19 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+    # success_url 커스터마이징 (datail은 pk가 필욯기때문에)
+    def get_success_url(self):
+        return reverse_lazy('accountapp:detail', kwargs={'pk' : self.object.user.pk})
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    # success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
+
+    # success_url 커스터마이징 (datail은 pk가 필욯기때문에)
+    def get_success_url(self):
+        return reverse_lazy('accountapp:detail', kwargs={'pk' : self.object.user.pk})
--- stay/urls.py
+++ stay/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
@@ -21,4 +23,4 @@ urlpatterns = [
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # setting에서 적어줫던 이미지 경로 라우팅해주기
