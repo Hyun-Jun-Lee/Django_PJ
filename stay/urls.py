27d179574d2f4180bd5db4c2b27999090618bae7
Lee Hyun Jun <mikey1000@naver.com>
2021-09-05 23:50:23 +0900

    commentapp start (FormMixin)

--- stay/urls.py
+++ stay/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
+    path('comments/', include('commentapp.urls')),
 
 
 
