846086ad7ef341a6b53acd240396f6b611ea8314
Lee Hyun Jun <mikey1000@naver.com>
2021-09-20 21:12:25 +0900

    make likerecord model

--- likeapp/models.py
+++ likeapp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+from articleapp.models import Article
 
-# Create your models here.
+
+class LikeRecord(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE , related_name='like_record')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='like_record')
+
+    class Meta:
+        unique_together = ('user', 'article')
