ab8ec578e169ef090d2a5b9559345e1d382dccab
Lee Hyun Jun <mikey1000@naver.com>
2022-03-22 22:49:44 +0900

    for secret key

--- stay/settings/deploy.py
+++ stay/settings/deploy.py
@@ -1,20 +1,9 @@
 
 from .base import *
-import os, json
-from django.core.exceptions import ImproperlyConfigured
+import os
+from decouple import config
 
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
 
 env = environ.Env(
     # set casting, default value
@@ -32,7 +21,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
