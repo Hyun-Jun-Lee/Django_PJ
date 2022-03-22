2bbb46af626f3672fc7210459696b8a8b87c581b
Lee Hyun Jun <mikey1000@naver.com>
2022-03-22 20:55:00 +0900

    seperate settings

--- stay/wsgi.py
+++ stay/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stay.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stay.settings.deploy')
 
 application = get_wsgi_application()
